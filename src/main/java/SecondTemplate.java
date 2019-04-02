bcdecb7ee689574b8c75a06bb81291c9654f6981
varus <flaviospano@gmail.com>
2019-04-02 12:26:16 +0200

    HMW2

--- src/main/java/SecondTemplate.java
+++ src/main/java/SecondTemplate.java
@@ -1,23 +1,43 @@
-            JavaPairRDD<String, Long> wordcountpairs = docs
-            // Map phase
-            .flatMapToPair((document) -> {
-              String[] tokens = document.split(" ");
-              HashMap<String, Long> counts = new HashMap<>();
-              ArrayList<Tuple2<String, Long>> pairs = new ArrayList<>();
-              for (String token : tokens) {
-                counts.put(token, 1L + counts.getOrDefault(token, 0L));
-              }
-              for (Map.Entry<String, Long> e : counts.entrySet()) {
-                pairs.add(new Tuple2<>(e.getKey(), e.getValue()));
-              }
-              return pairs.iterator();
-            })
-            // Reduce phase
-            .groupByKey()                       
-            .mapValues((it) -> {
-              long sum = 0;
-              for (long c : it) {
-                sum += c;
-              }
-              return sum;
-            });
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+
+public class FirstTemplate
+{
+
+    public static void main(String[] args) throws FileNotFoundException
+    {
+
+
+        JavaPairRDD<String, Long> wordcountpairs = docs
+                // Map phase
+                .flatMapToPair((document) -> {
+                    String[] tokens = document.split(" ");
+                    HashMap<String, Long> counts = new HashMap<>();
+                    ArrayList<Tuple2<String, Long>> pairs = new ArrayList<>();
+                    for (String token : tokens) {
+                        counts.put(token, 1L + counts.getOrDefault(token, 0L));
+                    }
+                    for (Map.Entry<String, Long> e : counts.entrySet()) {
+                        pairs.add(new Tuple2<>(e.getKey(), e.getValue()));
+                    }
+                    return pairs.iterator();
+                })
+                // Reduce phase
+                .groupByKey()
+                .mapValues((it) -> {
+                    long sum = 0;
+                    for (long c : it) {
+                        sum += c;
+                    }
+                    return sum;
+                });
+    }
+}
