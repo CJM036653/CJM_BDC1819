72eb4ee1af86da876870bc2c8c838afc17a1ccfe
CJM036653 <cjm036653@gmail.com>
2019-04-02 10:31:03 +0200

    Style edits to SecondTemplate.java

--- src/main/java/SecondTemplate.java
+++ src/main/java/SecondTemplate.java
@@ -8,33 +8,34 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 
-
-public class FirstTemplate
+public class SecondTemplate
 {
-
     public static void main(String[] args) throws FileNotFoundException
     {
-
-
         JavaPairRDD<String, Long> wordcountpairs = docs
                 // Map phase
-                .flatMapToPair((document) -> {
+                .flatMapToPair((document) ->
+                {
                     String[] tokens = document.split(" ");
                     HashMap<String, Long> counts = new HashMap<>();
                     ArrayList<Tuple2<String, Long>> pairs = new ArrayList<>();
-                    for (String token : tokens) {
+                    for (String token : tokens)
+                    {
                         counts.put(token, 1L + counts.getOrDefault(token, 0L));
                     }
-                    for (Map.Entry<String, Long> e : counts.entrySet()) {
+                    for (Map.Entry<String, Long> e : counts.entrySet())
+                    {
                         pairs.add(new Tuple2<>(e.getKey(), e.getValue()));
                     }
                     return pairs.iterator();
                 })
                 // Reduce phase
                 .groupByKey()
-                .mapValues((it) -> {
+                .mapValues((it) ->
+                {
                     long sum = 0;
-                    for (long c : it) {
+                    for (long c : it)
+                    {
                         sum += c;
                     }
                     return sum;
