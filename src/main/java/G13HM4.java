6a4cab739c8402570f58a3b584236090bb1b3820
CJM036653 <cjm036653@gmail.com>
2019-05-28 10:28:29 +0200

    HW4 - Functioning HW4 code.

--- src/main/java/G13HM4.java
+++ src/main/java/G13HM4.java
@@ -72,11 +72,12 @@ public class G13HM4
 
     public static Double MR_kmedian(JavaRDD<Vector> pointset, int k, int L, int iter)
     {
-        //
-        // --- ADD INSTRUCTIONS TO TAKE AND PRINT TIMES OF ROUNDS 1, 2 and 3 
-        //
+        long startTime = 0;
+        long endTime = 0;
 
         //------------- ROUND 1 ---------------------------
+        startTime = System.currentTimeMillis();
+
         JavaRDD<Tuple2<Vector,Long>> coreset = pointset.mapPartitions(x ->
         {
             ArrayList<Vector> points = new ArrayList<>();
@@ -97,7 +98,13 @@ public class G13HM4
             return c_w.iterator();
         });
 
+        coreset.cache();
+        coreset.count();
+        endTime = System.currentTimeMillis();
+        System.out.println("Round 1 of MR_kmedian requires " + (endTime - startTime) + " ms.");
+
         //------------- ROUND 2 ---------------------------
+        startTime = System.currentTimeMillis();
 
         ArrayList<Tuple2<Vector, Long>> elems = new ArrayList<>(k*L);
         elems.addAll(coreset.collect());
@@ -111,10 +118,31 @@ public class G13HM4
 
         ArrayList<Vector> centers = kmeansPP(coresetPoints, weights, k, iter);
 
-        //------------- ROUND 3: COMPUTE OBJ FUNCTION --------------------
-        //
-        //------------- ADD YOUR CODE HERE--------------------------------
-        // 
+        endTime = System.currentTimeMillis();
+        System.out.println("Round 2 of MR_kmedian requires " + (endTime - startTime) + " ms.");
+
+        //------------- ROUND 3: COMPUTE OBJ FUNCTION -------------------
+        startTime = System.currentTimeMillis();
+
+        double obj = pointset.map(x ->
+            {
+                double distance = Math.sqrt(Vectors.sqdist(centers.get(0), x));
+                for (int i = 1; i < centers.size(); i++)
+                {
+                    double newDistance = Math.sqrt(Vectors.sqdist(centers.get(i), x));
+                    if (newDistance < distance)
+                    {
+                        distance = newDistance;
+                    }
+                }
+                return distance;
+            })
+            .reduce((x,y) -> x+y);
+
+        endTime = System.currentTimeMillis();
+        System.out.println("Round 3 of MR_kmedian requires " + (endTime - startTime) + " ms.");
+
+        return obj/pointset.count();
     }
 
     public static ArrayList<Long> compute_weights(ArrayList<Vector> points, ArrayList<Vector> centers)
@@ -151,5 +179,137 @@ public class G13HM4
     // Euclidean distance
     public static double euclidean(Vector a, Vector b) {
         return Math.sqrt(Vectors.sqdist(a, b));
+    }
+
+
+    /* KmeansPP from Homework 3. */
+    public static ArrayList<Vector> kmeansPP(ArrayList<Vector> P, ArrayList<Long> WP, int k, int iter)
+    {
+        /* Internal class to store the distance of each point of P to C and the index of the corresponding centre. */
+        class Pair
+        {
+            Pair(double d, int i)
+            {
+                distance = d;
+                index = i;
+            }
+            double distance;
+            int index;
+        }
+
+        /* Set of centres. */
+        ArrayList<Vector> C = new ArrayList<>(k);
+
+        /********************************** KMEANS++ **********************************/
+        /* Add first random element with uniform distribution. */
+        Random randomGenerator = new Random();
+        int randomInt = randomGenerator.nextInt(P.size());
+        C.add(P.get(randomInt));
+
+        /* Store the minimum distance of all points in P from C. */
+        ArrayList<Pair> currentDistances = new ArrayList<>(P.size());
+        /* Compute the minimum distance of all points in P from the first centre. */
+        for (int j = 0; j < P.size(); j++)
+        {
+            currentDistances.add(new Pair(Math.sqrt(Vectors.sqdist(P.get(j), C.get(0))), 0));
+        }
+
+        for (int i = 1; i < k; i++)
+        {
+            /* Compute the weighted sum of distances of points in P from C. */
+            double sumOfDistances = 0;
+            for (int j = 0; j < currentDistances.size(); j++)
+            {
+                sumOfDistances += WP.get(j) * currentDistances.get(j).distance;
+            }
+            /* Choose a random value between 0 and 1. */
+            double chosenValue = randomGenerator.nextDouble();
+            double probabilitySum = 0;
+            int j = 0;
+            /* Find the next centre, chosen with the appropriate probability distribution. */
+            boolean done = false;
+            while (!done)
+            {
+                double currentProbability = (WP.get(j) * currentDistances.get(j).distance)/sumOfDistances;
+                double newProbabilitySum = probabilitySum + currentProbability;
+                /* If the current element is not already a centre... */
+                if (currentProbability != 0)
+                {
+                    /* If chosenValue is between probabilitySum and newProbabilitySum add the corresponding point to C and end the loop. */
+                    if (newProbabilitySum >= chosenValue)
+                    {
+                        C.add(P.get(j));
+                        done = true;
+                    }
+                }
+                probabilitySum = newProbabilitySum;
+                j++;
+            }
+
+            /* Recompute the minimum distance of points in P from C. */
+            for (j = 0; j < P.size(); j++)
+            {
+                double distance = Math.sqrt(Vectors.sqdist(P.get(j), C.get(i)));
+                if (distance < currentDistances.get(j).distance) currentDistances.set(j, new Pair(distance, i));
+            }
+        }
+
+        /********************************** LLOYD **********************************/
+        /* Perform iter iterations of LLoyd's algorithm. */
+        for (int i = 0; i < iter; i++)
+        {
+            /* ArrayList containing currently computed clusters as indexes of the points contained in P. */
+            ArrayList<ArrayList<Integer>> clusters = new ArrayList<>(k);
+
+            /* Partition(P,C) */
+            /* Create the clusters. */
+            for (int z = 0; z < k; z++)
+            {
+                clusters.add(new ArrayList<>());
+            }
+            /* Add each point to its cluster. */
+            for (int z = 0; z < P.size(); z++)
+            {
+                clusters.get(currentDistances.get(z).index).add(z);
+            }
+
+            /* Compute new centroids. */
+            for (int z = 0; z < k; z++)
+            {
+                Vector c = Vectors.zeros(P.get(0).size());
+                long sumOfWeights = 0;
+                for (int z0 = 0; z0 < clusters.get(z).size(); z0++)
+                {
+                    /* Compute the sum of all coordinates. */
+                    BLAS.axpy(WP.get(clusters.get(z).get(z0)), P.get(clusters.get(z).get(z0)), c);
+                    /* Compute the sum of weights. */
+                    sumOfWeights += WP.get(clusters.get(z).get(z0));
+                }
+
+                /* Compute the final average. */
+                BLAS.scal((1.0/sumOfWeights),c);
+                C.set(z, c);
+            }
+
+            /* Recompute currentDistances. */
+            for (int z = 0; z < P.size(); z++)
+            {
+                double minDistance = Math.sqrt(Vectors.sqdist(C.get(0), P.get(z)));
+                int minIndex = 0;
+                for (int z0 = 1; z0 < k; z0++)
+                {
+                    double distance = Math.sqrt(Vectors.sqdist(C.get(z0), P.get(z)));
+                    if (distance < minDistance)
+                    {
+                        minDistance = distance;
+                        minIndex = z0;
+                    }
+                }
+                currentDistances.get(z).distance = minDistance;
+                currentDistances.get(z).index = minIndex;
+            }
+        }
+
+        return C;
     }
 }
